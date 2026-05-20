3c1752ad3f1577df750f181d4182fb564a2db71d
Atsune Mogi <atsune@Atsunes-MacBook-Air-2.local>
2026-05-20 17:48:51 +0900

    add nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
-  };
+  }
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
