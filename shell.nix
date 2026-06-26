81d16bcd1948b25dc59d045a3be2f835ac9c0ff5
Atsune Mogi <atsune@Atsunes-MacBook-Air-2.local>
2026-06-26 12:44:58 +0900

    delete shell.nix

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  flake = builtins.getFlake (toString ./.);
-  system = builtins.currentSystem;
-in
-# Option A: If you are using the modern 'devShells' standard
-flake.devShells.${system}.default or flake.defaultApp.${system}
-
-# Option B (Fallback): If your flake defines it as 'devShell' (singular)
-# flake.devShell.${system}
